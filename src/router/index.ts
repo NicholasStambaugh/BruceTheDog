ba51f20ff50abcea58180993c2b1ebd92c18aeb4
Nick Stambaugh <nastambaugh@gmail.com>
2023-12-30 12:33:40 -0500

    more work on routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 import twentyThreeVue from '@/views/twenty-three.vue'
 import twentyTwoVue from '@/views/twenty-two.vue'
+import twentyOneVue from '@/views/twenty-one.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,9 +15,6 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     },
     {
@@ -26,8 +24,13 @@ const router = createRouter({
     },
     {
       path: '/twenty-two',
-      name: 'twenty-three',
+      name: 'twenty-two',
       component: twentyTwoVue
+    },
+    {
+      path: '/twenty-one',
+      name: 'twenty-one',
+      component: twentyOneVue
     }
   ]
 })
